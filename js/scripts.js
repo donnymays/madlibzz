b22ee4e977dea5056700142cac067a84eaed9306
donnymays <dtmays2@gmail.com>
2020-08-12 10:34:12 -0700

    finish js for code - it works!
    co-authored-by: Natalie Murphy <nataliemurphy500@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
-  $(#formOne).submit(function(event)){
-    
+  $("#formOne").submit(function(event) {
+    const person1Input = $("input#person1").val();
+    const item1Input = $("input#item1").val();
+    const kitchen_applianceInput = $("input#kitchen_appliance").val();
 
-    $("#person1").append(person1Input);
-    $("#item1").append(Item1Input);
-    $("kitchen_appliance").append(kitchen_applianceInput);
+    $(".person1").append(person1Input);
+    $(".item1").append(item1Input);
+    $(".kitchen_appliance").append(kitchen_applianceInput);
 
-    $("#story").Show();
+    $("#story").show();
 
     event.preventDefault();
   });
